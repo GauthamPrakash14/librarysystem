82eb996ff9854243657dceb0f0c2a824102e3027
GauthamPrakash14 <36693382+GauthamPrakash14@users.noreply.github.com>
2025-05-02 22:56:25 +0100

    User model

---
+++ src/main/java/com/example/librarymanagementsystem/model/User.java
@@ -0,0 +1,65 @@
+package com.example.librarymanagementsystem.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is mandatory")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    @Column(unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public enum Role{
+        ADMIN, LIBRARIAN, MEMBER
+    }
+
+    @PreUpdate
+    public void setUpdatedAt(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
